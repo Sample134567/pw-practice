de43b0c7240c0ac1ddc145af1e6ecf1f2dc79d63
pabs28 <pablosapitan28@gmail.com>
2024-11-29 23:34:50 +0800

    update the homepage_steps remove the duplicate steps

--- src/step-definitions/Homepage_Steps.ts
+++ src/step-definitions/Homepage_Steps.ts
@@ -16,15 +16,3 @@ When('I click on the login portal button', async () => {
   const login_button = await pageFixture.page.getByRole('link', { name: 'LOGIN PORTAL Login Portal' });
   await login_button.click();
 });
-
-When('I switch to new browser tab', async () => {
-  await pageFixture.context.waitForEvent('page');
-  
-  const allPages = await pageFixture.context.pages();
-
-  pageFixture.page = allPages[allPages.length - 1];
-
-  await pageFixture.page.bringToFront();
-
-  await pageFixture.page.setViewportSize( { width: 1920, height: 1080 } );
-});
